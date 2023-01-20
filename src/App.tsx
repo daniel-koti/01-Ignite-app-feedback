a7b1d6126eefdb09d9f183d98ffbe6133c227b4a
daniel-moniz <dkmoniz@cetim.ms>
2023-01-20 10:27:14 +0100

    fix: posts-array

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,12 @@
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
-import { Post, PostProps } from './components/Post'
+import { Post } from './components/Post'
 
 import styles from './App.module.css'
 
 import './global.css';
 
-interface Post extends PostProps {
-  id: number
-}
-
-const posts: Post []= [
-  {
-    id: 1,
-    author: {
-      avatarUrl: 'https://github.com/daniel-koti.png',
-      name: 'Daniel Moniz',
-      role: 'Software Engineer'
-    },
-    content: [
-      { type: 'paragraph', content: 'Fala galeraa 👋'},
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
-      { type: 'link', content: 'jane.design/doctorcare' },
-    ],
-    publishedAt: new Date('2022-08-29 20:00:00'),
-  },
-
-  {
-    id: 2,
-    author: {
-      avatarUrl: 'https://github.com/maykbrito.png',
-      name: 'Mayk Brito',
-      role: 'Educator @Rocketseat'
-    },
-    content: [
-      { type: 'paragraph', content: 'Fala galeraa 👋'},
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
-      { type: 'link', content: 'jane.design/doctorcare' },
-    ],
-    publishedAt: new Date('2022-08-29 19:00:00'),
-  },
-];
+import { posts } from './data'
 
 function App() {
   return (
---
+++ src/data.ts
@@ -0,0 +1,37 @@
+import { PostProps } from "./components/Post";
+
+interface Post extends PostProps {
+  id: number
+}
+
+export const posts: Post []= [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/daniel-koti.png',
+      name: 'Daniel Moniz',
+      role: 'Software Engineer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-08-29 20:00:00'),
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-08-29 19:00:00'),
+  },
+];
